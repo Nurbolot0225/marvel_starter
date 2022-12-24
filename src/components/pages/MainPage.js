312b1d5a3400b62d0084fad98899ea792d62edaa
Nurbolot Berdibekov <nurchik0225@gmail.com>
2022-12-24 13:50:23 +0600

    app seo optimized

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Helmet } from  "react-helmet";
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -18,6 +19,13 @@ const MainPage = () => {
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Marvel information portal"
+                    />
+                <title>Marvel information portal</title>
+            </Helmet>
             <ErrorBoundary>
                 <RandomChar/>
             </ErrorBoundary>
--- src/components/pages/singleComicLayout/SingleComicLayout.js
+++ src/components/pages/singleComicLayout/SingleComicLayout.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import './singleComicLayout.scss';
 
@@ -8,6 +9,13 @@ const SingleComicLayout = ({data}) => {
 
     return (
         <div className="single-comic">
+             <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} comics book`}
+                    />
+                <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
